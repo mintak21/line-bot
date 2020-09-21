4940859d42a80dc72808a791531fdd63bf43152a
mintak-Macbook <mintak21@gmail.com>
2020-09-21 19:03:01 +0900

    fix lint

--- example/python/server.py
+++ example/python/server.py
@@ -2,16 +2,10 @@ import logging
 import os
 
 from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
 
 app = Flask(__name__)
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
