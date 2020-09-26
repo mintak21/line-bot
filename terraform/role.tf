c18765bba9ac57766b5b7685b59b8cb09f0dac57
mintak-Macbook <mintak21@gmail.com>
2020-09-27 00:58:28 +0900

    fix roles

--- terraform/role.tf
+++ terraform/role.tf
@@ -4,17 +4,23 @@ resource google_service_account sa_for_cloud_run {
   description  = "Service Account For Cloud Run ${var.service_name}"
 }
 
-resource google_project_iam_member cloud_run_admin_role {
-  project = var.project_id
-  role    = "roles/run.admin"
-  member  = "serviceAccount:${google_service_account.sa_for_cloud_run.email}"
+resource google_service_account_iam_member cloud_run_iam {
+  service_account_id = google_service_account.sa_for_cloud_run.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${data.google_project.my_project.number}@cloudbuild.gserviceaccount.com"
 }
 
-resource google_project_iam_member cloud_run_sa_role {
-  project = var.project_id
-  role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${google_service_account.sa_for_cloud_run.email}"
-}
+# resource google_project_iam_member cloud_run_admin_role {
+#   project = var.project_id
+#   role    = "roles/run.admin"
+#   member  = "serviceAccount:${google_service_account.sa_for_cloud_run.email}"
+# }
+
+# resource google_project_iam_member cloud_run_sa_role {
+#   project = var.project_id
+#   role    = "roles/iam.serviceAccountUser"
+#   member  = "serviceAccount:${google_service_account.sa_for_cloud_run.email}"
+# }
 
 data google_iam_policy noauth {
   binding {
@@ -31,4 +37,7 @@ resource google_cloud_run_service_iam_policy noauth {
   service  = google_cloud_run_service.this.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+data google_project my_project {
 }
