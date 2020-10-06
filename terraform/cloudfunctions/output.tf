1474fb03c015c3cfbf365b2b639dd621479bfafa
mintak-Macbook <mintak21@gmail.com>
2020-10-06 23:05:04 +0900

    add output

--- terraform/cloudfunctions/output.tf
+++ terraform/cloudfunctions/output.tf
@@ -7,3 +7,8 @@ output this_google_cloudfunctions_function_url {
   description = "CloudFunctions接続先URL"
   value       = google_cloudfunctions_function.this.https_trigger_url
 }
+
+output service_key_google_kms_crypto_key_id {
+  description = "KMS KeyのID"
+  value       = google_kms_crypto_key.service_key.id
+}
