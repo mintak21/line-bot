54d133a7b76a7eff9d3f7e150f3cb3f6ebaeabac
mintak-Macbook <mintak21@gmail.com>
2020-10-23 22:08:35 +0900

    update google provider version

--- terraform/cloudrun/versions.tf
+++ terraform/cloudrun/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "3.42.0"
+      version = "3.44.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "3.42.0"
+      version = "3.44.0"
     }
   }
 }
